9290d56b1688eeab36930bc2a9c325f435f642f2
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-09-22 10:03:27 +0530

    Add files via upload

---
+++ Class and Objects/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int age;
+cout<<"enter the age of representative:";
+cin>>age;
+if (age>=18){
+     cout<<"eligible for voting";
+     }
+   else
+   {
+    cout<<"not eligible for voting";
+    }
+    return 0;
+    }
+
