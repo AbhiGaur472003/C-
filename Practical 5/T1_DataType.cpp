fdd54f7f275ec9ab671fdead0a478406f05e78f5
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-10-07 12:52:12 +0530

    Update T1_DataType.cpp

--- Practical 5/T1_DataType.cpp
+++ Practical 5/T1_DataType.cpp
@@ -1,1 +1,22 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int a=23;
+    float b=45.89;
+    char c='A';
+    bool d=true;
+    short f=2;
+    double g=23.45;
+    long doble h=345.9876;
+    wchar_t i='B';
+    cout<<"Integer: "<<a<<endl;
+    cout<<"Float: "<<b<<endl;
+    cout<<"Character: "<<c<<endl;
+    cout<<"Boolean: "<<d<<endl;
+    cout<<"Long: "<<e<<endl;
+    cout<<"Short: "<<f<<endl;
+    cout<<"Double: "<<g<<endl;
+    cout<<"Long double: "<<h<<endl;
+    cout<<"Wide Char: "<<i<<endl;
+    return 0;
+}
