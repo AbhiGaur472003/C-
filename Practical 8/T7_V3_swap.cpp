6bfa641daf9414bab355edf03c2b3340607f39dc
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-10-22 15:27:43 +0200

    Add files via upload

---
+++ Practical 8/T7_V3_swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+int main(){
+    int a=20;
+    int b=80;
+    cout<<"Value of a and b before swapping "<<a<<" "<<b<<endl;
+    swap(&a,&b);
+    cout<<"Value of a and b after swapping "<<a<<" "<<b<<endl;
+    return 0;
+}
