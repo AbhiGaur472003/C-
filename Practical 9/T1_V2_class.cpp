2fa9040780669d907d9da5446efccce99c080a74
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-11-05 09:30:50 +0530

    Update T1_V2_class.cpp

--- Practical 9/T1_V2_class.cpp
+++ Practical 9/T1_V2_class.cpp
@@ -1,23 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 class Student{
 public:
     string name;
-    int age;
     int year;
-    string address;
-    static int c;
-    void setA(string a){
-        c++;
+    char sec;
+    int marks;
+    static int sum;
+    void Set(string name,int year,char sec,int marks){
+        this->name=name;
+        this->year=year;
+        this->sec=sec;
+        this->marks=marks;
+    }
+    void Add(){
+        sum=sum+this->marks;
     }
 };
-int Student::c=0;
-int main()
-{
-    Student s[9];
-    for(int i=0;i<9;i++){
-        s[i].setA("Abhi");
-    }
-    cout<<"Total number of student in college: "<<Student::c<<endl;
-    return 0;
+int Student::sum;
+int main(){
+    Student s[3];
+    s[0].Set("Abhishek",2020,'d',59);
+    s[0].Add();
+    s[1].Set("Deepak",2019,'d',77);
+    s[1].Add();
+    s[2].Set("Ram",2018,'d',95);
+    s[2].Add();
+    cout<<Student::sum;
 }
