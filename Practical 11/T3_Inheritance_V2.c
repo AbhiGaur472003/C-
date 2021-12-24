c1106571c557b7f3be6ad68f9f0e0a47648823df
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-12-24 19:45:15 +0530

    Create T3_Inheritance_V2.c

---
+++ Practical 11/T3_Inheritance_V2.c
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Parent{
+public:
+    int a;
+    void Display1(){
+        cout<<"Calling Parent class"<<endl;
+    }
+};
+class Child1:virtual public Parent{
+public:
+    int b;
+    void Display2(){
+        cout<<"Calling Child1 Class"<<endl;
+    }
+};
+class Child2:virtual public Parent{
+public:
+    int c;
+    void Display3(){
+        cout<<"Calling Child2 Class"<<endl;
+    }
+};
+class Subchild:public Child1,public Child2{
+public:
+    int d;
+    void Display4(){
+        cout<<"Calling Subchild Class"<<endl;
+    }
+};
+int main(){
+    Subchild a;
+    a.Display1();//using virtual keyword to solve diamond problem...
+}
