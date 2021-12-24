2681a8b132a68a947632e8d1559c045d8993596f
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2021-12-24 15:12:14 +0100

    Add files via upload

---
+++ Practical 11/T1_Inheritance.c.c
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Parent{
+public:
+    void DisplayA(){
+        cout<<"Displaying Parent Class"<<endl;
+    }
+};
+class Child:public Parent{
+public:
+    void DisplayB(){
+        cout<<"Displaying Child Class"<<endl;
+        cout<<"Calling Parent class Function in Child class function"<<endl;
+        DisplayA();
+    }
+};
+int main()
+{
+    Child a;
+    a.DisplayB();
+    cout<<endl<<endl;
+    cout<<"Calling Directly the Parent class method using child object"<<endl;
+    a.DisplayA();
+    return 0;
+}
