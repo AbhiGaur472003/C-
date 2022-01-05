df2443c34b4dda0cc1b87254b9101ce1cc1946c0
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2022-01-05 16:15:41 +0530

    Update and rename T3_V1_Diamond.c to T3_V1_Diamond.cpp

--- Practical 11/T3_V1_Diamond.cpp
+++ Practical 11/T3_V1_Diamond.cpp
@@ -2,28 +2,24 @@
 using namespace std;
 class Parent{
 public:
-    int a;
     void Display1(){
         cout<<"Calling Parent class"<<endl;
     }
 };
 class Child1:public Parent{
 public:
-    int b;
     void Display2(){
         cout<<"Calling Child1 Class"<<endl;
     }
 };
 class Child2:public Parent{
 public:
-    int c;
     void Display3(){
         cout<<"Calling Child2 Class"<<endl;
     }
 };
 class Subchild:public Child1,public Child2{
 public:
-    int d;
     void Display4(){
         cout<<"Calling Subchild Class"<<endl;
     }
