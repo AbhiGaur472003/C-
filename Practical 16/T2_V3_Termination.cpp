a1df40c2b8641e2c1a6ef42b833a6c3b02738421
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2022-01-09 09:18:49 +0530

    Update T2_V3_Termination.cpp

--- Practical 16/T2_V3_Termination.cpp
+++ Practical 16/T2_V3_Termination.cpp
@@ -1,13 +1,3 @@
-// Output ----->   terminate called after throwing an instance of 'char'
-
-// ANS-->Because we are throwing exception of char instance but in the program
-//       there is no catch block which can catch char instance and there is 
-//       also no default catch block which can catch every type of exception.
-//       So when an exception is thrown and not caught anywhere , then the 
-//       program terminates abnormally.
-
-
-
 #include<iostream>
 using namespace std;
 int main(){
