36cbfe382d73a20d531d64a39a8e35ab188eb4c2
AbhiGaur472003 <89082799+AbhiGaur472003@users.noreply.github.com>
2022-01-09 09:18:31 +0530

    Update T2_V2_DefaultException.cpp

--- Practical 16/T2_V2_DefaultException.cpp
+++ Practical 16/T2_V2_DefaultException.cpp
@@ -1,10 +1,3 @@
-// Output ---->   Default Exception ...
-// ANS-->Because when we throw Exception of char instance then it will firstly find 
-//       where the char instance is catching but their is no catch block for char.
-//       catch(...) this will catch every type of exception.
-//       that's why catch(...) block will be executed.
-
-
 #include<iostream>
 using namespace std;
 int main(){
